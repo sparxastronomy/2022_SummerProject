493fbe8b2d0a4b3fce664cfdc77792978aba118e
Bipradeep Saha <bs19ms135@iiserkol.ac.in>
2022-05-24 17:01:06 +0530

    Added analysis of catalog data products.

--- Eagle/README.md
+++ Eagle/README.md
@@ -5,4 +5,8 @@ More organizational information will be updated as the project progress
 **NOTE:** The HDF5 files will not be uploaded to GitHub. One should run the notebook file with caution as some sections might download large files.
 
 - [`test.ipynb`](test.ipynb): As of now downloads the DM positonal data from Eagle `StrongFBL0025N0376` and `WeakFBL0025N0376`, plots the DM disttribution and computes the 2pCF. The accompanying pythton script file is also available for faster execution: [`plot.py`](plot.py).
-- [`querry.sh`](querry.sh): It is a shell script that queries the catalog data and save it to a `.csv` file.
+- [`querry.sh`](querry.sh): It is a shell script that queries the catalog data and save it to a `.csv` file.  
+The `.csv` are reasonable small ~80MB(for the biggest run) so you can download them. Make sure to remove the first few commented lines(lines starting with `#`) after saving of the `.csv` as the total output of the query is saved to these files and we don't need the query Metadata.
+- [`test_catalog.ipynb`](test_catalog.ipynb) : As of now computes the 2pCF of Eagle simulaiton for `StrongFBL0025N0376` `WeakFBL0025N0376`, `ViscLoL0050N0752`, `ViscHiL0050N0752` and `RefL0100N1504` from FoF halos.
+![2pCF for differnt variation of Eagle Run](FinalProducts/2pCF_Eagle_FoF.jpg)
+  
--- Eagle/querry.sh
+++ Eagle/querry.sh
@@ -1,1 +1,11 @@
-wget --http-user=pnm600 --http-passwd=CK205HQn -O StrongFBL0025N0376_FoF.csv "http://galaxy-catalogue.dur.ac.uk:8080/Eagle?action=doQuery&SQL=select GroupCentreOfPotential_x as X, GroupCentreOfPotential_y as Y, GroupCentreOfPotential_z as Z, GroupMass as Mass from Physics_vars..StrongFBL0025N0376_FoF"
+# Script to obtain catalog information from the EagleDB at z=0
+echo "Getting StrongFBL0025N0376_FoF"
+wget --http-user=pnm600 --http-passwd=CK205HQn --cookies=on --keep-session-cookies --save-cookies=cookie.txt --load-cookies=cookie.txt -O StrongFBL0025N0376_FoF.csv "http://galaxy-catalogue.dur.ac.uk:8080/Eagle?action=doQuery&SQL=select GroupCentreOfPotential_x as X, GroupCentreOfPotential_y as Y, GroupCentreOfPotential_z as Z, GroupMass as Mass from Physics_vars..StrongFBL0025N0376_FoF where SnapNum=28"
+echo "Getting WeakFBL0025N0376_FoF"
+wget --http-user=pnm600 --http-passwd=CK205HQn --cookies=on --keep-session-cookies --save-cookies=cookie.txt --load-cookies=cookie.txt -O WeakFBL0025N0376_FoF.csv "http://galaxy-catalogue.dur.ac.uk:8080/Eagle?action=doQuery&SQL=select GroupCentreOfPotential_x as X, GroupCentreOfPotential_y as Y, GroupCentreOfPotential_z as Z, GroupMass as Mass from Physics_vars..WeakFBL0025N0376_FoF where SnapNum=28"
+echo "Getting ViscHiL0050N0752_FoF"
+wget --http-user=pnm600 --http-passwd=CK205HQn --cookies=on --keep-session-cookies --save-cookies=cookie.txt --load-cookies=cookie.txt -O ViscHiL0050N0752_FoF.csv "http://galaxy-catalogue.dur.ac.uk:8080/Eagle?action=doQuery&SQL=select GroupCentreOfPotential_x as X, GroupCentreOfPotential_y as Y, GroupCentreOfPotential_z as Z, GroupMass as Mass from Physics_vars..ViscHiL0050N0752_FoF where SnapNum=28"
+echo "Getting ViscLoL0050N0752_FoF"
+wget --http-user=pnm600 --http-passwd=CK205HQn --cookies=on --keep-session-cookies --save-cookies=cookie.txt --load-cookies=cookie.txt -O ViscLoL0050N0752_FoF.csv "http://galaxy-catalogue.dur.ac.uk:8080/Eagle?action=doQuery&SQL=select GroupCentreOfPotential_x as X, GroupCentreOfPotential_y as Y, GroupCentreOfPotential_z as Z, GroupMass as Mass from Physics_vars..ViscLoL0050N0752_FoF where SnapNum=28"
+echo "Getting RefL0100N1504_FOF"
+wget --http-user=pnm600 --http-passwd=CK205HQn --cookies=on --keep-session-cookies --save-cookies=cookie.txt --load-cookies=cookie.txt -O RefL0100N1504_FOF.csv "http://galaxy-catalogue.dur.ac.uk:8080/Eagle?action=doQuery&SQL=select GroupCentreOfPotential_x as X, GroupCentreOfPotential_y as Y, GroupCentreOfPotential_z as Z, GroupMass as Mass from Eagle..RefL0100N1504_FOF where SnapNum=28"
